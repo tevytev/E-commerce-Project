aa58bf833c4c3635ca543932529fecd3e7dd2950
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2024-03-27 14:36:25 -0400

    new deploy

--- View/vite-view/src/main.jsx
+++ View/vite-view/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Route, Routes, Link, NavLink, useLocation } from 'react-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/E-commerce-Project/'>
         <Routes>
           <Route path='/*' element={<App />} />
         </Routes>
