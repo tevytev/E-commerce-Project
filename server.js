d0d769a280307f6a7cb04da3db2436debde1f8aa
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2024-04-02 20:37:25 -0400

    domain added to session

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.use(
       httpOnly: true,
       sameSite: 'lax',
       path: '/',
-      // domain: '.e-commerce-project-server.onrender.com'
+      domain: 'e-commerce-project-server.onrender.com'
     }
   })
 );
